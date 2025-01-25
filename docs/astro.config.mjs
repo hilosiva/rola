463f9255d7cf1b16a6cf96c1045969e686a6dd1f
Shibata Hironori <46587015+hilosiva@users.noreply.github.com>
2025-01-25 14:07:59 +0900

    add: site and base to astro.config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,46 +1,46 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Rola',
-			defaultLocale: 'root',
-			locales: {
+  site: "https://hilosiva.github.io",
+  base: "/rola",
+  integrations: [
+    starlight({
+      title: "Rola",
+      defaultLocale: "root",
+      locales: {
         root: {
-          label: '日本語',
-          lang: 'ja',
+          label: "日本語",
+          lang: "ja",
         },
       },
-			customCss: [
-        './src/assets/styles/global.css',
-			],
-			components: {
-        ContentPanel: './src/components/ContentPanel.astro',
+      customCss: ["./src/assets/styles/global.css"],
+      components: {
+        ContentPanel: "./src/components/ContentPanel.astro",
       },
-			social: {
-				github: 'https://github.com/hilosiva/rola',
-				'x.com': 'https://x.com/hilosiva',
-			},
-			sidebar: [
-				{
-					label: 'はじめる',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'インストール', slug: 'getting-started/installation' },
-						{ label: 'インビュー機能', slug: 'getting-started/inview' },
-						{ label: 'スクラブ機能', slug: 'getting-started/scrub' },
-						{ label: 'パフォーマンス', slug: 'getting-started/performance' },
-					],
-				},
-				{
-					label: 'リファレンス',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			lastUpdated: true,
-		}),
-	],
+      social: {
+        github: "https://github.com/hilosiva/rola",
+        "x.com": "https://x.com/hilosiva",
+      },
+      sidebar: [
+        {
+          label: "はじめる",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "インストール", slug: "getting-started/installation" },
+            { label: "インビュー機能", slug: "getting-started/inview" },
+            { label: "スクラブ機能", slug: "getting-started/scrub" },
+            { label: "パフォーマンス", slug: "getting-started/performance" },
+          ],
+        },
+        {
+          label: "リファレンス",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      lastUpdated: true,
+    }),
+  ],
 });
